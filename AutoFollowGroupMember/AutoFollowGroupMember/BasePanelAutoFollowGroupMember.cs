3b91fce7b05b1b33cf3784913f225001421dd838
jedzus <michalglowaczewski@gmail.com>
2015-05-22 04:39:20 +0200

    Auto Follow Group Member Plugin
    Auto Follow plugin for group member

---
+++ AutoFollowGroupMember/AutoFollowGroupMember/BasePanelAutoFollowGroupMember.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using Astral.Forms;
+using MyNW.Classes;
+using MyNW.Internals;
+using Timer = System.Threading.Timer;
+using TimerWF = System.Windows.Forms.Timer;
+
+namespace AutoFollowGroupMember
+{
+    public partial class BasePanelAutoFollowGroupMember : BasePanel
+    {
+        private Timer _followGroupMemberTimertimer;
+
+        public BasePanelAutoFollowGroupMember()
+            : base("Auto Follow")
+        {
+            InitializeComponent();
+            FirstUpdateComboBoxGroupMember();
+            UpdateComboBoxGroupMember();
+        }
+
+        private void FirstUpdateComboBoxGroupMember()
+        {
+            foreach (var entity in GetGroupMembersList())
+            {
+                comboBoxGroupMember.Items.Add(entity.Name);
+            }
+        }
+
+        private static IEnumerable<Entity> GetGroupMembersList()
+        {
+            return
+                EntityManager.GetEntities()
+                    .Where(
+                        entity =>
+                            entity.PlayerTeam.TeamId.Equals(EntityManager.LocalPlayer.PlayerTeam.TeamId) &&
+                            entity.Name != EntityManager.LocalPlayer.Name)
+                    .ToList();
+        }
+
+        private void UpdateComboBoxGroupMember()
+        {
+            var timer = new TimerWF();
+            timer.Tick += timer_Tick;
+            timer.Interval = 30000;
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            foreach (
+                var entity in GetGroupMembersList().Where(entity => !comboBoxGroupMember.Items.Contains(entity.Name)))
+            {
+                comboBoxGroupMember.Items.Add(entity.Name);
+            }
+        }
+
+        private Vector3 GetGroupMemberToFollowLocation()
+        {
+            if (comboBoxGroupMember.SelectedItem == null)
+                throw new Exception("Chose group member!");
+            var groupMemberToFollowLocation = Vector3.Empty;
+            foreach (
+                var entity in
+                    GetGroupMembersList().Where(entity => entity.Name == comboBoxGroupMember.SelectedItem.ToString()))
+            {
+                groupMemberToFollowLocation = entity.Location;
+            }
+            return groupMemberToFollowLocation;
+        }
+
+        private void FollowGroupMember(object state)
+        {
+            if (GetGroupMemberToFollowLocation() == null)
+                throw new Exception("Chose a group member!");
+            Movements.NavToPos(GetGroupMemberToFollowLocation(), 3f);
+            _followGroupMemberTimertimer.Change(500, Timeout.Infinite);
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _followGroupMemberTimertimer = new Timer(FollowGroupMember, null, 1000, Timeout.Infinite);
+            }
+            catch (Exception exception)
+            {
+                _followGroupMemberTimertimer.Dispose();
+                MessageBox.Show(exception.Message, "Auto Follow Group Member");
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            _followGroupMemberTimertimer.Dispose();
+        }
+    }
+}
---
+++ AutoFollowGroupMember/AutoFollowGroupMember/Config.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using Astral.Addons;
+using Astral.Forms;
+
+namespace AutoFollowGroupMember
+{
+    public class Config : Plugin
+    {
+        public override string Name
+        {
+            get { return "Auto Follow Group Member"; }
+        }
+
+        public override string Author
+        {
+            get { return "jedzus"; }
+        }
+
+        public override Image Icon
+        {
+            get { return null; }
+        }
+
+        public override BasePanel Settings
+        {
+            get { return new BasePanelAutoFollowGroupMember(); }
+        }
+
+        public override void OnLoad()
+        {
+            // not implemented
+        }
+
+        public override void OnUnload()
+        {
+            // not implemented
+        }
+
+        public override void OnBotStart()
+        {
+            // not implemented
+        }
+
+        public override void OnBotStop()
+        {
+            // not implemented
+        }
+    }
+}
