346f0a2661b440797b28e47e03373bcbf3773211
jedzus <michalglowaczewski@gmail.com>
2015-05-26 03:16:46 +0200

    Not tested plugins

---
+++ Not tested yet/DCHeal/DCHeal/Config.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using Astral.Addons;
+using Astral.Forms;
+
+namespace DCHeal
+{
+    public class Config : Plugin
+    {
+        public override void OnLoad()
+        {
+            //
+        }
+
+        public override void OnUnload()
+        {
+            //
+        }
+
+        public override void OnBotStart()
+        {
+            //
+        }
+
+        public override void OnBotStop()
+        {
+            //
+        }
+
+        public override string Name
+        {
+            get { return "DC Heal"; }
+        }
+
+        public override string Author
+        {
+            get { return "jedzus"; }
+        }
+
+        public override Image Icon
+        {
+            get { return null; }
+        }
+
+        public override BasePanel Settings
+        {
+            get { return new BasePanelDCHeal();}
+        }
+    }
+}
